a3570ab2d3e76bba494c9ad247d6cfb6ef8222f2
Francisco Fullenkamp <francisco000@gmail.com>
2020-05-10 23:22:19 -0400

    md: issuer_certificate option

--- lib/signer.rb
+++ lib/signer.rb
@@ -11,9 +11,9 @@ class Signer
   attr_reader :cert
   attr_writer :security_node, :signature_node, :security_token_id
 
-  WSU_NAMESPACE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd'
-  WSSE_NAMESPACE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd'
-  DS_NAMESPACE = 'http://www.w3.org/2000/09/xmldsig#'
+  WSU_NAMESPACE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+  WSSE_NAMESPACE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
+  DS_NAMESPACE = "http://www.w3.org/2000/09/xmldsig#"
 
   def initialize(document, noblanks: true)
     self.document = Nokogiri::XML(document.to_s) do |config|
@@ -58,38 +58,38 @@ class Signer
     @cert = certificate
     # Try to guess a digest algorithm for signature creation
     case @cert.signature_algorithm
-    when 'GOST R 34.11-94 with GOST R 34.10-2001'
+    when "GOST R 34.11-94 with GOST R 34.10-2001"
       self.signature_digest_algorithm = :gostr3411
-      self.signature_algorithm_id = 'http://www.w3.org/2001/04/xmldsig-more#gostr34102001-gostr3411'
+      self.signature_algorithm_id = "http://www.w3.org/2001/04/xmldsig-more#gostr34102001-gostr3411"
       # Add clauses for other types of keys that require other digest algorithms and identifiers
-      else # most common 'sha1WithRSAEncryption' type included here
-        self.set_default_signature_method! # Reset any changes as they can become malformed
-      end
+    else # most common 'sha1WithRSAEncryption' type included here
+      self.set_default_signature_method! # Reset any changes as they can become malformed
     end
+  end
 
-    def security_token_id
-      @security_token_id ||= "uuid-639b8970-7644-4f9e-9bc4-9c2e367808fc-1"
-    end
+  def security_token_id
+    @security_token_id ||= "uuid-639b8970-7644-4f9e-9bc4-9c2e367808fc-1"
+  end
 
-    def security_node
-      @security_node ||= document.xpath('//wsse:Security', wsse: WSSE_NAMESPACE).first
-    end
+  def security_node
+    @security_node ||= document.xpath("//wsse:Security", wsse: WSSE_NAMESPACE).first
+  end
 
-    def canonicalize(node = document, inclusive_namespaces=nil)
+  def canonicalize(node = document, inclusive_namespaces = nil)
     node.canonicalize(Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0, inclusive_namespaces, nil) # The last argument should be exactly +nil+ to remove comments from result
   end
 
   # <Signature xmlns="http://www.w3.org/2000/09/xmldsig#">
   def signature_node
     @signature_node ||= begin
-      @signature_node = security_node.at_xpath('ds:Signature', ds: DS_NAMESPACE)
-      unless @signature_node
-        @signature_node = Nokogiri::XML::Node.new('Signature', document)
-        set_namespace_for_node(@signature_node, DS_NAMESPACE, ds_namespace_prefix)
-        security_node.add_child(@signature_node)
+        @signature_node = security_node.at_xpath("ds:Signature", ds: DS_NAMESPACE)
+        unless @signature_node
+          @signature_node = Nokogiri::XML::Node.new("Signature", document)
+          set_namespace_for_node(@signature_node, DS_NAMESPACE, ds_namespace_prefix)
+          security_node.add_child(@signature_node)
+        end
+        @signature_node
       end
-      @signature_node
-    end
   end
 
   # <SignedInfo>
@@ -98,17 +98,17 @@ class Signer
   #   ...
   # </SignedInfo>
   def signed_info_node
-    node = signature_node.at_xpath('ds:SignedInfo', ds: DS_NAMESPACE)
+    node = signature_node.at_xpath("ds:SignedInfo", ds: DS_NAMESPACE)
     unless node
-      node = Nokogiri::XML::Node.new('SignedInfo', document)
+      node = Nokogiri::XML::Node.new("SignedInfo", document)
       signature_node.add_child(node)
       set_namespace_for_node(node, DS_NAMESPACE, ds_namespace_prefix)
-      canonicalization_method_node = Nokogiri::XML::Node.new('CanonicalizationMethod', document)
-      canonicalization_method_node['Algorithm'] = 'http://www.w3.org/2001/10/xml-exc-c14n#'
+      canonicalization_method_node = Nokogiri::XML::Node.new("CanonicalizationMethod", document)
+      canonicalization_method_node["Algorithm"] = "http://www.w3.org/2001/10/xml-exc-c14n#"
       node.add_child(canonicalization_method_node)
       set_namespace_for_node(canonicalization_method_node, DS_NAMESPACE, ds_namespace_prefix)
-      signature_method_node = Nokogiri::XML::Node.new('SignatureMethod', document)
-      signature_method_node['Algorithm'] = self.signature_algorithm_id
+      signature_method_node = Nokogiri::XML::Node.new("SignatureMethod", document)
+      signature_method_node["Algorithm"] = self.signature_algorithm_id
       node.add_child(signature_method_node)
       set_namespace_for_node(signature_method_node, DS_NAMESPACE, ds_namespace_prefix)
     end
@@ -127,26 +127,26 @@ class Signer
   #   </o:SecurityTokenReference>
   # </KeyInfo>
   def binary_security_token_node
-    node = document.at_xpath('wsse:BinarySecurityToken', wsse: WSSE_NAMESPACE)
+    node = document.at_xpath("wsse:BinarySecurityToken", wsse: WSSE_NAMESPACE)
     unless node
-      node = Nokogiri::XML::Node.new('BinarySecurityToken', document)
-      node['ValueType']    = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3'
-      node['EncodingType'] = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary'
-      node.content = Base64.encode64(cert.to_der).gsub("\n", '')
+      node = Nokogiri::XML::Node.new("BinarySecurityToken", document)
+      node["ValueType"] = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"
+      node["EncodingType"] = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary"
+      node.content = Base64.encode64(cert.to_der).gsub("\n", "")
       signature_node.add_previous_sibling(node)
-      wsse_ns = namespace_prefix(node, WSSE_NAMESPACE, 'wsse')
+      wsse_ns = namespace_prefix(node, WSSE_NAMESPACE, "wsse")
       #wsu_ns = namespace_prefix(node, WSU_NAMESPACE, 'wsu')
-      wsu_ns = namespace_prefix(node, WSU_NAMESPACE, '')
+      wsu_ns = namespace_prefix(node, WSU_NAMESPACE, "")
 
       #node["#{wsu_ns}:Id"] = security_token_id
       node["#{wsu_ns}:id"] = security_token_id
-      key_info_node = Nokogiri::XML::Node.new('KeyInfo', document)
+      key_info_node = Nokogiri::XML::Node.new("KeyInfo", document)
       security_token_reference_node = Nokogiri::XML::Node.new("#{wsse_ns}:SecurityTokenReference", document)
       key_info_node.add_child(security_token_reference_node)
       set_namespace_for_node(key_info_node, DS_NAMESPACE, ds_namespace_prefix)
       reference_node = Nokogiri::XML::Node.new("#{wsse_ns}:Reference", document)
-      reference_node['ValueType'] = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3'
-      reference_node['URI'] = "##{security_token_id}"
+      reference_node["ValueType"] = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"
+      reference_node["URI"] = "##{security_token_id}"
       security_token_reference_node.add_child(reference_node)
       signed_info_node.add_next_sibling(key_info_node)
     end
@@ -162,25 +162,25 @@ class Signer
   #     <X509Certificate>MIID+jCCAuKgAwIBAgIEAMdxxTANBgkqhkiG9w0BAQUFADBsMQswCQYDVQQGEwJTRTEeMBwGA1UEChMVTm9yZGVhIEJhbmsgQUIgKHB1YmwpMScwJQYDVQQDEx5Ob3JkZWEgcm9sZS1jZXJ0aWZpY2F0ZXMgQ0EgMDExFDASBgNVBAUTCzUxNjQwNi0wMTIwMB4XDTA5MDYxMTEyNTAxOVoXDTExMDYxMTEyNTAxOVowcjELMAkGA1UEBhMCU0UxIDAeBgNVBAMMF05vcmRlYSBEZW1vIENlcnRpZmljYXRlMRQwEgYDVQQEDAtDZXJ0aWZpY2F0ZTEUMBIGA1UEKgwLTm9yZGVhIERlbW8xFTATBgNVBAUTDDAwOTU1NzI0Mzc3MjCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwcgz5AzbxTbsCE51No7fPnSqmQBIMW9OiPkiHotwYQTl+H9qwDvQRyBqHN26tnw7hNvEShd1ZRGUg4drMEXDV5CmKqsAevs9lauWDaHnGKPNHZJ1hNNYXHwymksEz5zMnG8eqRdhb4vOV2FzreJeYpsgx31Bv0aTofHcHVz4uGcCAwEAAaOCASAwggEcMAkGA1UdEwQCMAAwEQYDVR0OBAoECEj6Y9/vU03WMBMGA1UdIAQMMAowCAYGKoVwRwEDMBMGA1UdIwQMMAqACEIFjfLBeTpRMDcGCCsGAQUFBwEBBCswKTAnBggrBgEFBQcwAYYbaHR0cDovL29jc3Aubm9yZGVhLnNlL1JDQTAxMA4GA1UdDwEB/wQEAwIGQDCBiAYDVR0fBIGAMH4wfKB6oHiGdmxkYXA6Ly9sZGFwLm5iLnNlL2NuPU5vcmRlYSUyMHJvbGUtY2VydGlmaWNhdGVzJTIwQ0ElMjAwMSxvPU5vcmRlYSUyMEJhbmslMjBBQiUyMChwdWJsKSxjPVNFP2NlcnRpZmljYXRlcmV2b2NhdGlvbmxpc3QwDQYJKoZIhvcNAQEFBQADggEBAEXUv87VpHk51y3TqkMb1MYDqeKvQRE1cNcvhEJhIzdDpXMA9fG0KqvSTT1e0ZI2r78mXDvtTZnpic44jX2XMSmKO6n+1taAXq940tJUhF4arYMUxwDKOso0Doanogug496gipqMlpLgvIhGt06sWjNrvHzp2eGydUFdCsLr2ULqbDcut7g6eMcmrsnrOntjEU/J3hO8gyCeldJ+fI81qarrK/I0MZLR5LWCyVG/SKduoxHLX7JohsbIGyK1qAh9fi8l6X1Rcu80v5inpu71E/DnjbkAZBo7vsj78zzdk7KNliBIqBcIszdJ3dEHRWSI7FspRxyiR0NDm4lpyLwFtfw=</X509Certificate>
   #   </X509Data>
   # </KeyInfo>
-  def x509_data_node
-    issuer_name_node   = Nokogiri::XML::Node.new('X509IssuerName', document)
-    issuer_name_node.content = cert.issuer.to_s[1..-1].gsub(/\//, ',')
+  def x509_data_node(issuer_certificate_in_security_token = true)
+    issuer_name_node = Nokogiri::XML::Node.new("X509IssuerName", document)
+    issuer_name_node.content = cert.issuer.to_s[1..-1].gsub(/\//, ",")
 
-    issuer_number_node = Nokogiri::XML::Node.new('X509SerialNumber', document)
+    issuer_number_node = Nokogiri::XML::Node.new("X509SerialNumber", document)
     issuer_number_node.content = cert.serial
 
-    issuer_serial_node = Nokogiri::XML::Node.new('X509IssuerSerial', document)
+    issuer_serial_node = Nokogiri::XML::Node.new("X509IssuerSerial", document)
     issuer_serial_node.add_child(issuer_name_node)
     issuer_serial_node.add_child(issuer_number_node)
 
-    cetificate_node    = Nokogiri::XML::Node.new('X509Certificate', document)
-    cetificate_node.content = Base64.encode64(cert.to_der).gsub("\n", '')
+    cetificate_node = Nokogiri::XML::Node.new("X509Certificate", document)
+    cetificate_node.content = Base64.encode64(cert.to_der).gsub("\n", "")
 
-    data_node          = Nokogiri::XML::Node.new('X509Data', document)
-    data_node.add_child(issuer_serial_node)
+    data_node = Nokogiri::XML::Node.new("X509Data", document)
+    data_node.add_child(issuer_serial_node) if issuer_certificate_in_security_token
     data_node.add_child(cetificate_node)
 
-    key_info_node      = Nokogiri::XML::Node.new('KeyInfo', document)
+    key_info_node = Nokogiri::XML::Node.new("KeyInfo", document)
     key_info_node.add_child(data_node)
 
     signed_info_node.add_next_sibling(key_info_node)
@@ -219,7 +219,7 @@ class Signer
   def digest!(target_node, options = {})
     wsu_ns = namespace_prefix(target_node, WSU_NAMESPACE)
     #current_id = target_node["#{wsu_ns}:Id"]  if wsu_ns
-    current_id = target_node["#{wsu_ns}:id"]  if wsu_ns
+    current_id = target_node["#{wsu_ns}:id"] if wsu_ns
 
     id = options[:id] || current_id || "_#{Digest::SHA1.hexdigest(target_node.to_s)}"
     if id.to_s.size > 0
@@ -232,36 +232,36 @@ class Signer
     target_canon = canonicalize(target_node, options[:inclusive_namespaces])
     target_digest = Base64.encode64(@digester.digest(target_canon)).strip
 
-    reference_node = Nokogiri::XML::Node.new('Reference', document)
-    reference_node['URI'] = id.to_s.size > 0 ? "##{id}" : ""
+    reference_node = Nokogiri::XML::Node.new("Reference", document)
+    reference_node["URI"] = id.to_s.size > 0 ? "##{id}" : ""
     signed_info_node.add_child(reference_node)
     set_namespace_for_node(reference_node, DS_NAMESPACE, ds_namespace_prefix)
 
-    transforms_node = Nokogiri::XML::Node.new('Transforms', document)
+    transforms_node = Nokogiri::XML::Node.new("Transforms", document)
     reference_node.add_child(transforms_node)
     set_namespace_for_node(transforms_node, DS_NAMESPACE, ds_namespace_prefix)
 
-    transform_node = Nokogiri::XML::Node.new('Transform', document)
+    transform_node = Nokogiri::XML::Node.new("Transform", document)
     set_namespace_for_node(transform_node, DS_NAMESPACE, ds_namespace_prefix)
     if options[:enveloped]
-      transform_node['Algorithm'] = 'http://www.w3.org/2000/09/xmldsig#enveloped-signature'
+      transform_node["Algorithm"] = "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
     else
-      transform_node['Algorithm'] = 'http://www.w3.org/2001/10/xml-exc-c14n#'
+      transform_node["Algorithm"] = "http://www.w3.org/2001/10/xml-exc-c14n#"
     end
     if options[:inclusive_namespaces]
-      inclusive_namespaces_node = Nokogiri::XML::Node.new('ec:InclusiveNamespaces', document)
-      inclusive_namespaces_node.add_namespace_definition('ec', transform_node['Algorithm'])
-      inclusive_namespaces_node['PrefixList'] = options[:inclusive_namespaces].join(' ')
+      inclusive_namespaces_node = Nokogiri::XML::Node.new("ec:InclusiveNamespaces", document)
+      inclusive_namespaces_node.add_namespace_definition("ec", transform_node["Algorithm"])
+      inclusive_namespaces_node["PrefixList"] = options[:inclusive_namespaces].join(" ")
       transform_node.add_child(inclusive_namespaces_node)
     end
     transforms_node.add_child(transform_node)
 
-    digest_method_node = Nokogiri::XML::Node.new('DigestMethod', document)
-    digest_method_node['Algorithm'] = @digester.digest_id
+    digest_method_node = Nokogiri::XML::Node.new("DigestMethod", document)
+    digest_method_node["Algorithm"] = @digester.digest_id
     reference_node.add_child(digest_method_node)
     set_namespace_for_node(digest_method_node, DS_NAMESPACE, ds_namespace_prefix)
 
-    digest_value_node = Nokogiri::XML::Node.new('DigestValue', document)
+    digest_value_node = Nokogiri::XML::Node.new("DigestValue", document)
     digest_value_node.content = target_digest
     reference_node.add_child(digest_value_node)
     set_namespace_for_node(digest_value_node, DS_NAMESPACE, ds_namespace_prefix)
@@ -284,23 +284,23 @@ class Signer
     end
 
     if options[:issuer_serial]
-      x509_data_node
+      x509_data_node(options[:issuer_certificate])
     end
 
     if options[:inclusive_namespaces]
-      c14n_method_node = signed_info_node.at_xpath('ds:CanonicalizationMethod', ds: DS_NAMESPACE)
-      inclusive_namespaces_node = Nokogiri::XML::Node.new('ec:InclusiveNamespaces', document)
-      inclusive_namespaces_node.add_namespace_definition('ec', c14n_method_node['Algorithm'])
-      inclusive_namespaces_node['PrefixList'] = options[:inclusive_namespaces].join(' ')
+      c14n_method_node = signed_info_node.at_xpath("ds:CanonicalizationMethod", ds: DS_NAMESPACE)
+      inclusive_namespaces_node = Nokogiri::XML::Node.new("ec:InclusiveNamespaces", document)
+      inclusive_namespaces_node.add_namespace_definition("ec", c14n_method_node["Algorithm"])
+      inclusive_namespaces_node["PrefixList"] = options[:inclusive_namespaces].join(" ")
       c14n_method_node.add_child(inclusive_namespaces_node)
     end
 
     signed_info_canon = canonicalize(signed_info_node, options[:inclusive_namespaces])
 
     signature = private_key.sign(@sign_digester.digester, signed_info_canon)
-    signature_value_digest = Base64.encode64(signature).gsub("\n", '')
+    signature_value_digest = Base64.encode64(signature).gsub("\n", "")
 
-    signature_value_node = Nokogiri::XML::Node.new('SignatureValue', document)
+    signature_value_node = Nokogiri::XML::Node.new("SignatureValue", document)
     signature_value_node.content = signature_value_digest
     signed_info_node.add_next_sibling(signature_value_node)
     set_namespace_for_node(signature_value_node, DS_NAMESPACE, ds_namespace_prefix)
@@ -312,7 +312,7 @@ class Signer
   # Reset digest algorithm for signature creation and signature algorithm identifier
   def set_default_signature_method!
     self.signature_digest_algorithm = :sha1
-    self.signature_algorithm_id = 'http://www.w3.org/2000/09/xmldsig#rsa-sha1'
+    self.signature_algorithm_id = "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
   end
 
   ##
@@ -323,7 +323,6 @@ class Signer
   # adds such a namespace to +target_node+ with +desired_prefix+
 
   def namespace_prefix(target_node, namespace, desired_prefix = nil)
-
     ns = target_node.namespaces.key(namespace)
     if ns
       ns.match(/(?:xmlns:)?(.*)/) && $1
